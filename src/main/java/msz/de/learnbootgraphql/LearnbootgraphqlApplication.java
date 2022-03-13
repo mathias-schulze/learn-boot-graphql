1634ef22dedbd89fdc1b8b9a3932c41799fb307e
Mathias Schulze <mathias-schulze@web.de>
2022-03-13 01:24:56 +0100

    Spring Initializr with GraphQL

---
+++ src/main/java/msz/de/learnbootgraphql/LearnbootgraphqlApplication.java
@@ -0,0 +1,13 @@
+package msz.de.learnbootgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnbootgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnbootgraphqlApplication.class, args);
+	}
+
+}
